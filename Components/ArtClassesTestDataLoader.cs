6b2f923aa8300be57e26db1d575e7477746b036e
ejohn033 <ejohn033@odu.edu>
2024-11-18 09:44:54 -0600

    added loader strategy test class for loading all classes

---
+++ Components/ArtClassesTestDataLoader.cs
@@ -0,0 +1,52 @@
+﻿
+namespace ArtStudioManager.Components
+{
+    public class ArtClassesTestDataLoader : IDataLoader<ICollection<ArtClass>>
+    {
+        private Random _random = new();
+        private Array _classTypes = Enum.GetValues(typeof(ClassType));
+        private Array _classNames = new[] {
+            "Record Acrylic Pour", "Acrylic Canvas", "Bowl Pottery", "Finnish Bracelet", "Dog Scratch", "Coffee Face" };
+
+        public Task Load(ICollection<ArtClass> artClasses)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                var artClass = new ArtClass();
+                artClass.Type = (ClassType)_classTypes.GetValue(_random.Next(_classTypes.Length))!;
+                artClass.Name = (string)_classNames.GetValue(_random.Next(_classNames.Length))!;
+                artClass.Description = "Any added description details";
+                artClass.Start = DateTime.Now.AddDays(_random.Next(10));
+                artClass.End = artClass.Start.AddHours(2);
+                artClass.Instructors = new List<Instructor> { new Instructor() { Name = "Karen", IsPrimary = true } };
+
+                artClass.Attendees = new List<Artist> {
+                    new Member() { Name = "Eric" },
+                    new Member() { Name = "Paula" },
+                    new Member() { Name = "Carla" },
+                    new Member() { Name = "Linda" },
+                    new NonMember() { Name = "Bob" },
+                    new NonMember() { Name = "Steve" },
+                    new NonMember() { Name = "Trevor" },
+                };
+
+                artClass.Cost = (decimal)_random.Next(100);
+                artClass.MemberDiscount = new FlatRateDiscount((decimal)_random.Next(100));
+
+                artClass.Materials = new List<Material> {
+                    new() { Name = "Some Material", Quantity = 2.5f },
+                    new() { Name = "Second Material", Quantity = 12.5f },
+                    new() { Name = "Other Material", Quantity = 33f },
+                    new() { Name = "Last Material", Quantity = 10.25f }
+                };
+
+                artClass.Attendance = new Attendance();
+                artClass.Attendance.AddAttendees(artClass.Attendees);
+
+                artClasses.Add(artClass);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
