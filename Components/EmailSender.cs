653bc91a05e624edcfa2abe5420ddea4021fd914
ejohn033 <ejohn033@odu.edu>
2024-11-19 10:37:28 -0600

    added static email sender, added subject and body text areas to send emails page, along with send emails button

---
+++ Components/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Net.Mail;
+
+namespace ArtStudioManager.Components
+{
+    public static class EmailSender
+    {
+        private static readonly string _userName = ""; //add in borealis username and password when testing
+        private static readonly string _password = "";
+
+        public static void SendEmails(
+            ICollection<string> emailAddresses, string fromAddress, string fromAddressDisplayName, string subject, string body)
+        {
+            SmtpClient client = new SmtpClient("smtp-mail.outlook.com", 587);
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(_userName, _password);
+            client.EnableSsl = true;
+            client.Credentials = credentials;
+            MailAddress from = new MailAddress(fromAddress, fromAddressDisplayName, System.Text.Encoding.UTF8);
+            MailAddress to;
+            MailMessage message;
+
+            foreach (string emailAddress in emailAddresses)
+            {
+                to = new MailAddress(emailAddress);
+                message = new MailMessage(from, to);
+                message.SubjectEncoding = System.Text.Encoding.UTF8;
+                message.Subject = subject;
+                message.Body = body;
+                client.Send(message);
+                message.Dispose();
+            }
+
+            client.Dispose();
+        }
+    }
+}
