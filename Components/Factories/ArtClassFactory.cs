13ae6e3cb1e44db7391e6a77dbd6f9c537df4112
ericdouglasjohnson32@gmail.com <ericdouglasjohnson32@gmail.com>
2024-12-18 15:53:40 -0600

    quick change to see what user is pushing commit

--- Components/Factories/ArtClassFactory.cs
+++ Components/Factories/ArtClassFactory.cs
@@ -6,6 +6,11 @@ namespace ArtStudioManager.Components.Factories
 {
     public static class ArtClassFactory
     {
+        /// <summary>
+        /// For creating a new ArtClass.
+        /// The unique id of the ArtClass is automatically generated.
+        /// </summary>
+        /// <returns>ArtClass</returns>
         public static ArtClass Create()
         {
 
